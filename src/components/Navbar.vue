37c8ef50c4d6f5551f6f75d1e3575495af9d9fc2
Imam Hossain Roni <imamhossainroni95@gmail.com>
2019-02-22 14:15:00 +0600

    item search done

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,8 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <form @submit="search" class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" v-model="keyword" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
     </div>
@@ -17,7 +17,17 @@
 
 <script>
     export default {
+data(){
+  return{
+keyword:''
+  }
+},
+ methods:{
+   search(keyword){
+     this.$emit("search",this.keyword)
+   }
 
+ }
     }
 </script>
 
