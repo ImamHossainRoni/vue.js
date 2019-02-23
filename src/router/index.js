9a15c0ef7ce0cd72a0a0c0d07dc76dc9985fbcb9
Imam Hossain Roni <imamhossainroni95@gmail.com>
2019-02-23 22:58:58 +0600

    vue-route worked

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Test from '../components/views/Test';
+import Inventory from '../components/views/Inventory'
+import ItemDetails from '../components/views/ItemDetails'
+export const routes=[{
+  path:'/',
+  component:Inventory
+},
+  {
+    path:'/test/:id',
+    component:Test
+  },
+  {
+    path:'/item/:id',
+    component:ItemDetails
+  }
+  ]
